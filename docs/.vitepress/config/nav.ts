58edb29ea573f61579d3a01294b9990e72d9213b
sujit-168 <su2054552689@gmail.com>
2023-10-14 16:30:54 +0800

    ci: Use self-hosted runners

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -5,14 +5,29 @@ export const nav: DefaultTheme.Config['nav'] = [
     { text: '首页', link: '/' },
     { text: '官网', link: 'https://www.tianbot.com' },
     {
-        text: '相关文档',
-        activeMatch: '^/(use_guide|basic_guide|simulation|enhance)/',
+        text: '产品文档',
         items: [
-            { text: '基础知识', link: '/basic_guide/' },
-            { text: '仿真模拟', link: '/simulation/' },
-            { text: '拓展提升', link: '/enhance/' },
+            { text: 'ROS2GO文档', link: '/use_guide/ros2go/' },
+            { text: 'TOM系列文档', link: '/use_guide/tianbot/' },
+            { text: 'Tianrover文档', link: '/use_guide/tianrover/' },
+            { text: 'Tianracer文档', link: '/use_guide/tianracer/' },
+            { text: 'Tianbot mini文档', link: '/use_guide/tianbot_mini/' },
+            { text: 'Robomaster TT文档', link: '/use_guide/rmtt/' },
+            { text: 'ROSECHO文档', link: '/use_guide/rosecho/' },
         ],
     },
+    { text: '基础知识', link: '/basic_guide/' },
+    { text: '仿真模拟', link: '/simulation/' },
+    { text: '拓展提升', link: '/enhance/' },
+    // {
+    //     text: '相关文档',
+    //     activeMatch: '^/(use_guide|basic_guide|simulation|enhance)/',
+    //     items: [
+    //         { text: '基础知识', link: '/basic_guide/' },
+    //         { text: '仿真模拟', link: '/simulation/' },
+    //         { text: '拓展提升', link: '/enhance/' },
+    //     ],
+    // },
 
     { text: '关于我们', link: 'https://www.tianbot.com/page?id=4' },
 ];
