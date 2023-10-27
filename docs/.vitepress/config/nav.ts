74548f51c4f6b583e95474b98176f4a292f9e960
sujit-168 <su2054552689@gmail.com>
2023-10-27 15:23:54 +0800

    docs: add section of competition

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -5,7 +5,7 @@ export const nav: DefaultTheme.Config['nav'] = [
     { text: '首页', link: '/' },
     { text: '官网', link: 'https://www.tianbot.com' },
     {
-        text: '相关文档',
+        text: '产品文档',
         items: [
             { text: 'ROS2GO文档', link: '/manual/ros2go/' },
             { text: 'TOM系列文档', link: '/manual/tianbot/' },
@@ -18,6 +18,14 @@ export const nav: DefaultTheme.Config['nav'] = [
     },
     { text: '基础知识', link: '/basic/' },
     { text: '仿真模拟', link: '/simulation/' },
+    { 
+        text: '竞赛专项',
+        items: [ 
+            {text: '空地协同比赛', link: '/competition/air_ground_synergy/' },
+            {text: 'F1TENTH 线上模拟赛', link: '/competition/f1tenth_online/' },
+            {text: 'F1TENTH 线下挑战赛', link: '/competition/f1tenth_offline/' }
+        ]
+    },
     { text: '拓展提升', link: '/advanced/' },
     { text: '关于我们', link: 'https://www.tianbot.com/page?id=4' },
 ];
